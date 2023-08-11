967270660aef8e1cd2521f00ca718fd5a9a9a483
Daniel Tolan <danieltolan@gmail.com>
2023-08-11 10:19:46 -1000

    Add icon for lua dap. Bind t keys to hop.nvim

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -103,8 +103,8 @@ return {
       "jbyuki/one-small-step-for-vimkind",
       -- stylua: ignore
       keys = {
-        { "<leader>daL", function() require("osv").launch({ port = 8086 }) end, desc = "Adapter Lua Server" },
-        { "<leader>dal", function() require("osv").run_this() end, desc = "Adapter Lua" },
+        { "<leader>daL", function() require("osv").launch({ port = 8086 }) end, desc = "Lua Server 󰢱 " },
+        { "<leader>dal", function() require("osv").run_this() end, desc = "Lua 󰢱 " },
       },
       config = function()
         local dap = require("dap")
--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -29,5 +29,11 @@ return {
     vim.keymap.set("", "F", function()
       hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
     end, { remap = true })
+    vim.keymap.set("", "t", function()
+      hop.hint_char2({ direction = directions.AFTER_CURSOR, current_line_only = false })
+    end, { remap = true })
+    vim.keymap.set("", "T", function()
+      hop.hint_char2({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+    end, { remap = true })
   end,
 }
