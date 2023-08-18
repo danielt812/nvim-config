02bae768e3ad58aef589ddfca03eec6edb9b1e4e
Daniel Tolan <danieltolan@gmail.com>
2023-08-18 08:37:48 -1000

    use system clipboard instead of tmux clipboard

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -24,7 +24,7 @@ return {
         -- overwrites vim.g.clipboard to redirect * and + to the system
         -- clipboard using tmux. If you sync your system clipboard without tmux,
         -- disable this option!
-        sync_clipboard = true,
+        sync_clipboard = false,
 
         -- synchronizes registers *, +, unnamed, and 0 till 9 with tmux buffers.
         sync_registers = true,
