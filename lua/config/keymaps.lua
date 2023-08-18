dd30d7e6e39e8a4c8f2990bcb8007038f521d4ed
Daniel Tolan <danieltolan@gmail.com>
2023-08-18 08:13:08 -1000

    remove gg/G keymaps, send substitute to blackhole register

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -58,8 +58,8 @@ map({ "n", "v", "x" }, "x", '"_x', { desc = "Prevent x from yanking to clipboard
 map({ "n", "v", "x" }, "X", '"_X', { desc = "Prevent X from yanking to clipboard" })
 map({ "n", "v", "x" }, "c", '"_c', { desc = "Prevent c from yanking to clipboard" })
 map({ "n", "v", "x" }, "C", '"_C', { desc = "Prevent C from yanking to clipboard" })
--- map({ "n", "v", "x" }, "s", '"_s', { desc = "Prevent s from yanking to clipboard" })
--- map({ "n", "v", "x" }, "S", '"_S', { desc = "Prevent S from yanking to clipboard" })
+map({ "n", "v", "x" }, "s", '"_s', { desc = "Prevent s from yanking to clipboard" })
+map({ "n", "v", "x" }, "S", '"_S', { desc = "Prevent S from yanking to clipboard" })
 
 -- Add undo break-points
 map("i", ",", ",<c-g>u")
@@ -72,10 +72,6 @@ map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>w<CR><esc>", { desc = "Save file" })
 -- Better indenting
 map("v", "<", "<gv", { desc = "Indent block to left", silent = true })
 map("v", ">", ">gv", { desc = "Indent block to right", silent = true })
-
--- Always go to beginning of line when using gg or G
-map("n", "gg", "gg0", { desc = "Go to beginning of file" })
-map("n", "G", "G0", { desc = "Go to end of file" })
 
 -- Yanky
 map({ "n", "x" }, "p", "<Plug>(YankyPutAfter)", { desc = "Yanky put after" })
