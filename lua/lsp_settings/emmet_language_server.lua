0282cd51ab8a381e03b40ccc88de4b90099a3454
Daniel Tolan <danieltolan@gmail.com>
2023-08-24 13:50:19 -1000

    cleanup emmet lsp config

--- lua/lsp_settings/emmet_language_server.lua
+++ lua/lsp_settings/emmet_language_server.lua
@@ -1,7 +1,7 @@
 return {
+  cmd = { "emmet-language-server", "--stdio" },
   filetypes = {
     "css",
-    "eruby",
     "html",
     "javascript",
     "javascriptreact",
@@ -13,20 +13,5 @@ return {
     "typescriptreact",
     "vue",
   },
-  init_options = {
-    --- @type table<string, any> https://docs.emmet.io/customization/preferences/
-    preferences = {},
-    --- @type "always" | "never" defaults to `"always"`
-    showexpandedabbreviation = "always",
-    --- @type boolean defaults to `true`
-    showabbreviationsuggestions = true,
-    --- @type boolean defaults to `false`
-    showsuggestionsassnippets = false,
-    --- @type table<string, any> https://docs.emmet.io/customization/syntax-profiles/
-    syntaxprofiles = {},
-    --- @type table<string, string> https://docs.emmet.io/customization/snippets/#variables
-    variables = {},
-    --- @type string[]
-    excludelanguages = {},
-  },
+  single_file_support = true,
 }
