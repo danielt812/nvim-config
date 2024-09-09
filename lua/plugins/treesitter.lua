c7097f5f59132d0c234c079049f320d91b291e47
Daniel Tolan <danieltolan@gmail.com>
2024-09-09 12:27:00 -1000

    treesitter not respecting ignore_install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,38 +5,40 @@ M.enabled = true
 M.event = { "BufRead", "BufNewFile" }
 
 M.cmd = {
-	"TSInstall",
-	"TSUninstall",
-	"TSUpdate",
-	"TSUpdateSync",
-	"TSInstallInfo",
-	"TSInstallSync",
-	"TSInstallFromGrammar",
+  "TSInstall",
+  "TSUninstall",
+  "TSUpdate",
+  "TSUpdateSync",
+  "TSInstallInfo",
+  "TSInstallSync",
+  "TSInstallFromGrammar",
 }
 
-M.config = function()
-	local opts = {
-		ensure_installed = require("parsers"),
+M.opts = function()
+  return {
+    ensure_installed = require("parsers"),
 
-		-- Install parsers synchronously (only applied to `ensure_installed`)
-		sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-		-- Automatically install missing parsers when entering buffer
-		auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    auto_install = false,
 
-		-- List of parsers to ignore installing (for "all")
-		ignore_install = { "csv" },
+    -- List of parsers to ignore installing (for "all")
+    ignore_install = { "csv" },
 
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		indent = {
-			enable = false,
-		},
-	}
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = false,
+    },
+  }
+end
 
-	require("nvim-treesitter.configs").setup(opts)
+M.config = function(_, opts)
+  require("nvim-treesitter.configs").setup(opts)
 end
 
 return M
