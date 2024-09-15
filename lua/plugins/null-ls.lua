1dd23a19ebd0e0831c302aeb79d8c511b22f82ab
Daniel Tolan <danieltolan@gmail.com>
2024-09-14 21:42:00 -1000

    format with spaces instead of tabs

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -7,53 +7,53 @@ M.dependencies = { "nvim-lua/plenary.nvim" }
 M.event = { "BufReadPre" }
 
 M.keys = {
-	{ "<leader>lin", "<cmd>NullLsInfo<cr>", "<cmd>Null-LS 󱆨 " },
+  { "<leader>lin", "<cmd>NullLsInfo<cr>", "<cmd>Null-LS 󱆨 " },
 }
 
 M.opts = function()
-	local formatting = require("null-ls").builtins.formatting
-	local diagnostics = require("null-ls").builtins.diagnostics
+  local formatting = require("null-ls").builtins.formatting
+  local diagnostics = require("null-ls").builtins.diagnostics
 
-	return {
-		debug = false,
-		sources = {
-			formatting.prettierd.with({
-				extra_filetypes = { "toml" },
-				extra_args = {
-					"--trailing-comma=es5",
-					"--use-tabs",
-					"--tab-width=2",
-					-- "--single-quote",
-					"--jsx-single-quote",
-					"--print-width=120",
-					"--single-attribute-per-line",
-				},
-			}),
-			-- formatting.biome.with({
-			--   extra_args = {
-			--     "--trailing-comma=es5",
-			--     "--indent-style=space",
-			--     "--indent-width=2",
-			--     "--jsx-quote-style=single",
-			--     "--line-width=120",
-			--     "--semicolons=always",
-			--   },
-			-- }),
-			formatting.stylua,
-			formatting.shfmt.with({
-				filetypes = { "zsh", "bash", "sh" },
-			}),
-			formatting.stylua.with({
-				extra_args = {
-					"--config-path=stylua.toml",
-				},
-			}),
-		},
-	}
+  return {
+    debug = false,
+    sources = {
+      formatting.prettierd.with({
+        extra_filetypes = { "toml" },
+        extra_args = {
+          "--trailing-comma=es5",
+          -- "--use-tabs",
+          "--tab-width=2",
+          -- "--single-quote",
+          "--jsx-single-quote",
+          "--print-width=120",
+          "--single-attribute-per-line",
+        },
+      }),
+      -- formatting.biome.with({
+      --   extra_args = {
+      --     "--trailing-comma=es5",
+      --     "--indent-style=space",
+      --     "--indent-width=2",
+      --     "--jsx-quote-style=single",
+      --     "--line-width=120",
+      --     "--semicolons=always",
+      --   },
+      -- }),
+      formatting.stylua,
+      formatting.shfmt.with({
+        filetypes = { "zsh", "bash", "sh" },
+      }),
+      formatting.stylua.with({
+        extra_args = {
+          "--config-path=stylua.toml",
+        },
+      }),
+    },
+  }
 end
 
 M.config = function(_, opts)
-	require("null-ls").setup(opts)
+  require("null-ls").setup(opts)
 end
 
 return M
