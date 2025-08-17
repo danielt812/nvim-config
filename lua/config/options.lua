e024ddebf5c4b94babb55e4c098d5d22ed1ee4f3
Daniel Tolan <danieltolan@gmail.com>
2025-08-16 21:15:32 -1000

    options - use vim.opt instead of vim.o

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,19 +1,21 @@
-vim.o.autoindent = true
-vim.o.expandtab = true
-vim.o.clipboard = "unnamedplus"
-vim.o.completeopt = "menuone,noselect,fuzzy"
-vim.o.expandtab = true
-vim.o.laststatus = 2
-vim.o.list = true
-vim.o.relativenumber = true
-vim.o.scrolloff = 10
-vim.o.shiftwidth = 2
-vim.o.swapfile = false
-vim.o.tabstop = 2
-vim.o.updatetime = 1000
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = "menuone,noselect,fuzzy"
+vim.opt.expandtab = true
+vim.opt.laststatus = 2
+vim.opt.list = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
+vim.opt.shiftwidth = 2
+vim.opt.swapfile = false
+vim.opt.tabstop = 2
+vim.opt.updatetime = 1000
+
+vim.opt.foldcolumn = "0"
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
 
 vim.opt.fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "", lastline = " " }
-vim.o.foldcolumn = "0"
-vim.o.foldenable = true
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
+vim.opt.shortmess:append("S")
