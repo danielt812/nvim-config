97148f31207829c35ab365e1ed9b8863df9849e3
Daniel Tolan <danieltolan@gmail.com>
2025-09-21 10:01:49 +0200

    auto trim whitespace on save

--- lua/mini_plugins/trailspace.lua
+++ lua/mini_plugins/trailspace.lua
@@ -3,3 +3,14 @@ local trailspace = require("mini.trailspace")
 trailspace.setup({
   only_in_normal_buffers = true,
 })
+
+local au = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+au("BufWritePre", {
+  group = augroup("minitrail_space_trim_on_save", { clear = true }),
+  pattern = "*",
+  callback = function()
+    trailspace.trim()
+  end,
+})
