1763988bc28579416bb8dae7e481366be9a76954
Daniel Tolan <danieltolan@gmail.com>
2025-09-21 10:02:03 +0200

    treesj increase max join length

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -24,7 +24,7 @@ end
 treesj.setup({
   use_default_keymaps = false,
   check_syntax_error = true,
-  max_join_length = 120,
+  max_join_length = 1000,
   cursor_behavior = "hold",
   notify = true,
   dot_repeat = true,
