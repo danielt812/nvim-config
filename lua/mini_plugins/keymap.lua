72f3c62b0cecc70b5d7d66591ba56b11dafe61b0
Daniel Tolan <danieltolan@gmail.com>
2025-11-12 06:45:36 -1000

    update mini.keymap tab steps

--- lua/mini_plugins/keymap.lua
+++ lua/mini_plugins/keymap.lua
@@ -6,7 +6,7 @@ local map_combo     = keymap.map_combo
 -- stylua: ignore end
 
 local tab_steps = {
-  -- "minisnippets_next",
+  "minisnippets_next",
   -- "pmenu_next",
   "increase_indent",
   "minisnippets_expand",
@@ -16,12 +16,13 @@ local tab_steps = {
 map_multistep("i", "<Tab>", tab_steps)
 
 local shifttab_steps = {
-  -- "minisnippets_prev",
+  "minisnippets_prev",
   -- "pmenu_prev",
   "decrease_indent",
   "jump_before_tsnode",
   "jump_before_open",
 }
+
 map_multistep("i", "<S-Tab>", shifttab_steps)
 -- Use same keybinds as fzf
 -- i for regular completion, c for wildmenu
@@ -37,6 +38,20 @@ map_multistep("i", "<BS>", { "nvimautopairs_bs", "minipairs_bs" })
 local no_hlsearch = function() vim.cmd("nohlsearch") end
 
 map_combo({ "n", "i", "x", "c" }, "<Esc><Esc>", no_hlsearch)
+
+map_combo("i", "<", function()
+  local line = vim.api.nvim_get_current_line()
+  local col = vim.fn.col(".")
+  -- stylua: ignore
+  if line:sub(col - 2, col) == "<<>" then return "<Del>" end
+end)
+
+map_combo("i", "=", function()
+  local line = vim.api.nvim_get_current_line()
+  local col = vim.fn.col(".")
+  -- stylua: ignore
+  if line:sub(col - 2, col) == "<=>" then return "<Del>" end
+end)
 
 -- Less intrusive hardtime?
 -- local notify_many_keys = function(key)
