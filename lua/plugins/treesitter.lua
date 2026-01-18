7522b55b6b39318cc96ead59f7371a31bacf07eb
Daniel Tolan <danieltolan@gmail.com>
2026-01-17 19:33:56 -1000

    migrate to treesitter main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,34 +1,47 @@
-local treesitter = require("nvim-treesitter.configs")
+local treesitter = require("nvim-treesitter")
 
 treesitter.setup({
-  ensure_installed = {
-    "bash",
-    "c",
-    "comment",
-    "css",
-    "html",
-    "javascript",
-    "jsdoc",
-    "json",
-    "lua",
-    "markdown",
-    "query",
-    "regex",
-    "scss",
-    "toml",
-    "tsx",
-    "typescript",
-    "vim",
-    "vimdoc",
-    "yaml",
-  },
-  ignore_install = { "csv" },
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = false,
-  },
+  install_dir = vim.fn.stdpath("data") .. "/site",
+})
+
+local languages = {
+  "bash",
+  "c",
+  "comment",
+  "css",
+  "html",
+  "javascript",
+  "jsdoc",
+  "json",
+  "lua",
+  "markdown",
+  "query",
+  "regex",
+  "scss",
+  "toml",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "yaml",
+  "zsh",
+}
+
+treesitter.install(languages)
+
+local filetypes = {}
+
+for _, language in ipairs(languages) do
+  for _, filetype in ipairs(vim.treesitter.language.get_filetypes(language)) do
+    table.insert(filetypes, filetype)
+  end
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = filetypes,
+  group = vim.api.nvim_create_augroup("treesitter", { clear = true }),
+  desc = "Start treesitter",
+  callback = function(args)
+    vim.treesitter.start(args.buf)
+  end,
 })
