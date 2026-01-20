57a4db95bf5c6e10f095d86ab16fc8d4456956d4
Daniel T <danieltolan@gmail.com>
2026-01-19 19:25:20 -1000

    mini.pairs - trying new neigh patterns

--- lua/mini_plugins/pairs.lua
+++ lua/mini_plugins/pairs.lua
@@ -2,31 +2,30 @@ local pairs = require("mini.pairs")
 
 pairs.setup({
   modes = {
-    insert = false,
+    insert = true,
     command = true,
     terminal = true,
   },
   mappings = {
     -- stylua: ignore start
-    ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
-    ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
-    ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+    ["("] = { action = "open", pair = "()", neigh_pattern = ".[^%w_\\]" },
+    ["["] = { action = "open", pair = "[]", neigh_pattern = ".[^%w_\\]" },
+    ["{"] = { action = "open", pair = "{}", neigh_pattern = ".[^%w_\\]" },
 
     [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
     ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
     ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
 
-    ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].",   register = { cr = false } },
-    ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
-    ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].",   register = { cr = false } },
+    ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%w_][^%w_]",   register = { cr = false } },
+    ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%w_\\][^%w_]", register = { cr = false } },
+    ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^%w_][^%w_]",   register = { cr = false } },
     -- stylua: ignore end
   },
 })
 
--- local function toggle_pairs()
---   vim.b.minipairs_disable = not vim.b.minipairs_disable
+local function toggle_pairs()
+  vim.b.minipairs_disable = not vim.b.minipairs_disable
+  vim.notify("Pairs " .. (vim.b.minipairs_disable and "disabled" or "enabled"), vim.log.levels.INFO)
+end
 
---   vim.notify("Pairs " .. (vim.b.minipairs_disable and "disabled" or "enabled"), vim.log.levels.INFO)
--- end
-
--- vim.keymap.set("n", "<leader>ep", toggle_pairs, { desc = "Pairs" })
+vim.keymap.set("n", "<leader>ep", toggle_pairs, { desc = "Pairs" })
