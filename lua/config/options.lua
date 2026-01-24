8eb259b1144c79890d4f7eab7c7833b88b0bc6c7
Daniel T <danieltolan@gmail.com>
2026-01-23 15:49:33 -1000

    update options - pumheight, grepprg, guicursor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,8 @@ vim.opt.autoindent = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.completeopt = "menuone,noselect,fuzzy"
 vim.opt.expandtab = true
-vim.opt.laststatus = 0
 vim.opt.list = true
+vim.opt.pumheight = 10
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 vim.opt.shiftwidth = 2
@@ -12,10 +12,15 @@ vim.opt.tabstop = 2
 vim.opt.updatetime = 1000
 vim.opt.winborder = "rounded"
 
+vim.opt.grepprg =  "rg --vimgrep -u --glob '!.git/*' --glob '!node_modules/*' --glob '!.angular/*'"
+
 vim.opt.foldcolumn = "0"
 vim.opt.foldenable = true
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 
+
 vim.opt.fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "", lastline = " " }
+
+vim.opt.guicursor:append("a:blinkon500-blinkoff500")
 vim.opt.shortmess:append("S")
