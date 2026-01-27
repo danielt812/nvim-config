7d81bf54f2b2c809cf20b47f3e04a2393f1b5a6a
Daniel Tolan <danieltolan@gmail.com>
2026-01-26 18:16:37 -1000

    update custom Grep cmd

--- lua/config/quickfix.lua
+++ lua/config/quickfix.lua
@@ -1,16 +1,14 @@
--- vim.api.nvim_create_user_command("Grep", function(opts)
---   -- opts.fargs is already shell-parsed and quote-aware
---   local escaped = vim.tbl_map(vim.fn.shellescape, opts.fargs)
+vim.api.nvim_create_user_command("Grep", function(opts)
+  local escaped = vim.tbl_map(vim.fn.shellescape, opts.fargs)
 
---   local cmd = "silent grep! " .. table.concat(escaped, " ")
+  local cmd = "silent grep! " .. table.concat(escaped, " ")
 
---   vim.cmd(cmd)
---   vim.cmd("copen")
--- end, {
---   nargs = "+",
---   desc = "ripgrep (safe, supports flags, respects ripgrep.conf)",
--- })
-
+  vim.cmd(cmd)
+  vim.cmd("copen")
+end, {
+  nargs = "+",
+  desc = "ripgrep (safe, supports flags, respects ripgrep.conf)",
+})
 
 local function clear_qf_list()
   vim.cmd("cexpr []")
@@ -36,3 +34,19 @@ vim.keymap.set("n", "<leader>qd", set_diagnostics, { desc = "Diagnostics" })
 vim.keymap.set("n", "<leader>qq", toggle_qf_list,  { desc = "Quickfix" })
 vim.keymap.set("n", "<leader>qc", clear_qf_list,   { desc = "Clear" })
 -- stylua: ignore end
+
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+  group = vim.api.nvim_create_augroup("quickfix_grep", { clear = true }),
+  pattern = { "grep", "lgrep" },
+  callback = function()
+    vim.cmd("copen")
+    vim.cmd("wincmd j")
+  end,
+})
+
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+  group = vim.api.nvim_create_augroup("quickfix_grep", { clear = true }),
+  callback = function()
+    vim.notify("QuickFixCmdPost event")
+  end,
+})
