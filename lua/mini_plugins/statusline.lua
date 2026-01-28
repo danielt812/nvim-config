695666869805235d4baf09fb5f41364031967a65
Daniel Tolan <danieltolan@gmail.com>
2026-01-27 16:52:14 -1000

    tinker with mini.statusline

--- lua/mini_plugins/statusline.lua
+++ lua/mini_plugins/statusline.lua
@@ -5,24 +5,17 @@ local components = require("utils.components")
 statusline.setup({
   content = {
     active = function()
-      -- NOTE:  these are custom highlight groups that are not part of mini.nvim
-      local diagnostic_icons =
-        { ERROR = "", WARN = "", INFO = "", HINT = "" }
-
+      -- NOTE  these are custom highlight groups that are not part of mini.nvim
+      local diagnostic_levels = { ERROR = "", WARN = "", INFO = "", HINT = "" }
       local diagnostic_symbols = {}
 
-      for severity, icon in pairs(diagnostic_icons) do
-        diagnostic_symbols[severity] = string.format(
-          "%%#MiniStatuslineDiag%s#%s %%#MiniStatuslineDevinfo#",
-          severity,
-          icon
-        )
+      for severity, icon in pairs(diagnostic_levels) do
+        diagnostic_symbols[severity] =
+          string.format("%%#MiniStatuslineDiag%s#%s %%#MiniStatuslineDevinfo#", severity, icon)
       end
 
       local function section_disabled_mods(trunc_width)
-        if statusline.is_truncated(trunc_width) then
-          return ""
-        end
+        if statusline.is_truncated(trunc_width) then return "" end
         local pairs = components.pairs({ text = true })
         local prefix = pairs ~= "" and "󱐤" or ""
         return prefix .. pairs
@@ -35,12 +28,7 @@ statusline.setup({
 
         local ft = vim.bo.filetype or "none"
         local icon, hl = icons.get("filetype", ft)
-        local fileinfo = string.format(
-          "%%#%s#%s %%#MiniStatuslineFileinfo#%s%%*",
-          hl or "",
-          icon,
-          ft
-        )
+        local fileinfo = string.format("%%#%s#%s %%#MiniStatuslineFileinfo#%s%%*", hl or "", icon, ft)
 
         return truncate and fileinfo or spell .. shift .. fileinfo
       end
@@ -53,19 +41,27 @@ statusline.setup({
 
       local function section_progress(trunc_width)
         local truncate = statusline.is_truncated(trunc_width)
-        local percent = "%P"
         local progressbar = components.progressbar({ pad = "left" })
 
-        return truncate and percent or percent .. progressbar
+        return truncate and "%P" or "%P" .. progressbar
+      end
+
+      local function section_filename(trunc_width)
+        local truncate = statusline.is_truncated(trunc_width)
+
+        if vim.bo.buftype == "terminal" then
+          return "%t"
+        else
+          return truncate and "%f" or "%F"
+        end
       end
 
       local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
       local git = statusline.section_git({ trunc_width = 100 })
       local diff = statusline.section_diff({ trunc_width = 100, icon = "" })
-      -- stylua: ignore
       local diagnostics = statusline.section_diagnostics({ trunc_width = 75, icon = "", signs = diagnostic_symbols })
-      local filename = statusline.section_filename({ trunc_width = 240 })
       local searchcount = statusline.section_searchcount({ trunc_width = 80 })
+      local filename = section_filename(120)
       local disabled = section_disabled_mods(80)
       local fileinfo = section_fileinfo(120)
       local location = section_location(100)
