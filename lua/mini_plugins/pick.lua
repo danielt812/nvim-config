20d045e02fa6141cfd4dff2d5072a457dcb3fe49
Daniel Tolan <danieltolan@gmail.com>
2026-01-29 19:25:03 -1000

    mini.pick - update highlight groups picker keymap

--- lua/mini_plugins/pick.lua
+++ lua/mini_plugins/pick.lua
@@ -22,7 +22,7 @@ vim.keymap.set("n", "<leader>fe", "<cmd>Pick explorer<cr>",     { desc = "Explor
 vim.keymap.set("n", "<leader>ff", "<cmd>Pick files<cr>",        { desc = "Files" })
 vim.keymap.set("n", "<leader>fg", "<cmd>Pick grep_live<cr>",    { desc = "Livegrep" })
 vim.keymap.set("n", "<leader>fh", "<cmd>Pick help<cr>",         { desc = "Help" })
-vim.keymap.set("n", "<leader>fi", "<cmd>Pick hl_groups<cr>",    { desc = "Highlights" })
+vim.keymap.set("n", "<leader>fl", "<cmd>Pick hl_groups<cr>",    { desc = "Highlights" })
 vim.keymap.set("n", "<leader>fk", "<cmd>Pick keymaps<cr>",      { desc = "Keymaps" })
 vim.keymap.set("n", "<leader>fm", "<cmd>Pick marks<cr>",        { desc = "Marks" })
 -- stylua: ignore end
