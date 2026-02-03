3280a5a6edac8f24c8127b411da386f15d6620ff
Daniel Tolan <danieltolan@gmail.com>
2026-02-02 20:40:05 -1000

    work on yank module

--- lua/modules/yank.lua
+++ lua/modules/yank.lua
@@ -33,7 +33,7 @@ ModuleYank.config = {
   },
 
   keyring = {
-    enabled = true,
+    only_meaningful = true,
   },
 }
 
@@ -42,10 +42,7 @@ H.default_config = vim.deepcopy(ModuleYank.config)
 
 H.cache = {
   cursor_pos = nil,
-}
-
-H.history = {
-  items = {},
+  history = {},
 }
 
 -- Helper functionality --------------------------------------------------------
@@ -57,7 +54,7 @@ H.setup_config = function(config)
   H.check_type("yank.preserve_cursor", config.yank.preserve_cursor, "boolean")
 
   H.check_type("keyring", config.keyring, "table")
-  H.check_type("keyring.enabled", config.keyring.enabled, "boolean")
+  H.check_type("keyring.only_meaningful", config.keyring.only_meaningful, "boolean")
 
   return config
 end
@@ -72,6 +69,8 @@ H.apply_config = function(config)
     H.map({ "n", "v" }, "Y", preserve_Y, { expr = true })
   end
 
+  H.map("n", "<leader>mc", function() print(vim.inspect(H.cache)) end, { desc = "Print module cache" })
+
   H.apply_autocommands(config)
 end
 
@@ -82,16 +81,45 @@ H.get_map_info = function(mode, lhs)
   end
 end
 
+H.get_config = function(config)
+  return vim.tbl_deep_extend("force", ModuleYank.config, vim.b.moduleyank_config or {}, config or {})
+end
+
 -- Yank ------------------------------------------------------------------------
 H.preserve_cursor = function(key)
-  H.cache.cursor_pos = vim.api.nvim_win_get_cursor(0)
+  local pos = vim.fn.getpos("v")
+  local anchor = { pos[2], pos[3] - 1 } -- [bufnum, lnum, col, off]
+  H.cache.cursor_position = anchor
   return key
 end
 
-H.get_event_reg = function()
-  local r = vim.v.event.regname
-  return (r ~= nil and r ~= "") and r or '"'
+H.append_yank = function()
+  local register = H.get_event_register()
+  local contents = H.get_event_contents()
+
+  if H.get_config().keyring.only_meaningful then
+    -- no blanks
+    if vim.trim(H.stringify(contents)) == "" then return end
+    -- no duplicates
+    for _, item in ipairs(H.cache.history) do
+      if item.register == register and H.stringify(item.contents) == H.stringify(contents) then return end
+    end
+  end
+
+  table.insert(H.cache.history, { register = register, contents = contents })
 end
+
+H.get_event_contents = function()
+  local contents = vim.v.event.regcontents
+  return contents
+end
+
+H.get_event_register = function()
+  local register = vim.v.event.regname
+  return register ~= "" and register or '"'
+end
+
+H.stringify = function(lines) return table.concat(lines, "\n") end
 
 H.get_reg_lines = function(reg) return vim.fn.getreg(reg, 1) end
 
@@ -107,16 +135,19 @@ H.apply_autocommands = function(config)
 
   if config.yank.preserve_cursor then
     local preserve_cursor = function()
-      if H.is_yank_event and H.cache.cursor_pos then
-        vim.api.nvim_win_set_cursor(0, H.cache.cursor_pos)
-        H.cache.cursor_pos = nil
+      if H.is_yank_event and H.cache.cursor_position then
+        vim.api.nvim_win_set_cursor(0, H.cache.cursor_position)
+        H.cache.cursor_position = nil
       end
     end
     au("TextYankPost", "*", preserve_cursor, "Preserve cursor position on yank")
   end
 
-  if config.keyring.enabled then
-    -- au("TextYankPost", "*", update_hist_cache, "Save yank to history file")
+  if config.keyring.only_meaningful then
+    local update_history = function()
+      if H.is_yank_event then H.append_yank() end
+    end
+    au("TextYankPost", "*", update_history, "Save yank to history file")
   end
 end
 
