80c45a374de94666c5383a387ba322b61474dd8c
Daniel Tolan <danieltolan@gmail.com>
2026-02-10 20:04:51 -1000

    map use 4x2 dots

--- lua/mini_plugins/map.lua
+++ lua/mini_plugins/map.lua
@@ -1,22 +1,22 @@
 local map = require("mini.map")
 
 map.setup({
+  symbols = {
+    encode = map.gen_encode_symbols.dot("4x2"),
+    scroll_line = "▶ ",
+    scroll_view = "┃ ",
+  },
   integrations = {
     map.gen_integration.builtin_search(),
     map.gen_integration.diff(),
     map.gen_integration.diagnostic(),
   },
-  symbols = {
-    encode = map.gen_encode_symbols.dot("3x2"),
-    scroll_line = "▶ ",
-    scroll_view = "┃ ",
-  },
 })
 
 vim.keymap.set("n", "<leader>em", map.toggle, { desc = "Map" })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("mini_map", { clear = true }),
-  desc = "Auto open mini map",
-  callback = function(args) end,
-})
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   group = vim.api.nvim_create_augroup("mini_map", { clear = true }),
+--   desc = "Auto open mini map",
+--   callback = map.open,
+-- })
