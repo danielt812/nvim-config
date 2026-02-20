544be1345c0aee3a875fd6725c731506714bbb88
Daniel Tolan <danieltolan@gmail.com>
2026-02-20 04:37:58 -1000

    mini.indentscope - update predicate func

--- lua/mini_plugins/indentscope.lua
+++ lua/mini_plugins/indentscope.lua
@@ -5,17 +5,18 @@ indentscope.setup({
   draw = {
     -- Delay (in ms) between event and start of drawing scope indicator
     delay = 100,
-
-    -- Animation rule for scope's first drawing. A function which, given
-    -- next and total step numbers, returns wait time (in ms). See
-    -- |MiniIndentscope.gen_animation| for builtin options. To disable
-    -- animation, use `require('mini.indentscope').gen_animation.none()`.
     animation = indentscope.gen_animation.quadratic({ easing = "out", duration = 100, unit = "total" }),
 
     -- Whether to auto draw scope: return `true` to draw, `false` otherwise.
     -- Default draws only fully computed scope (see `options.n_lines`).
-    -- stylua: ignore
-    predicate = function(scope) return not scope.body.is_incomplete end,
+    predicate = function(scope)
+      local ft_ignore = { "help", "markdown" }
+      if vim.tbl_contains(ft_ignore, vim.bo[scope.buf_id].filetype) then
+        return false
+      else
+        return not scope.body.is_incomplete
+      end
+    end,
 
     -- Symbol priority. Increase to display on top of more symbols.
     priority = 2,
@@ -48,7 +49,7 @@ indentscope.setup({
     -- Whether to first check input line to be a border of adjacent scope.
     -- Use it if you want to place cursor on function header to get scope of
     -- its body.
-    try_as_border = false,
+    try_as_border = true,
   },
 
   -- Which character to use for drawing scope indicator
