8f12841ad2194dd3c65271c609b657974d9ef26f
Daniel Tolan <danieltolan@gmail.com>
2026-02-20 15:31:49 -1000

    treesj - prefer mini.splitjoin only when cursor is on pair char

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -11,7 +11,7 @@ treesj.setup({
 
 local splitjoin = require("mini.splitjoin")
 
-local in_arrow_function = function()
+local on_arrow_function = function()
   local ok, node = pcall(vim.treesitter.get_node)
   if not ok or not node then return false end
 
@@ -22,16 +22,31 @@ local in_arrow_function = function()
   return false
 end
 
+local on_pair_char = function()
+  local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line = vim.api.nvim_get_current_line()
+
+  -- char under cursor
+  local c1 = line:sub(col + 1, col + 1)
+  -- char just left of cursor (useful when cursor is between chars)
+  local c0 = (col > 0) and line:sub(col, col) or ""
+
+  -- treat these as "pair chars"
+  local pairs = { ["("] = true, [")"] = true, ["{"] = true, ["}"] = true, ["["] = true, ["]"] = true }
+
+  return pairs[c1] == true or pairs[c0] == true
+end
+
 local split_join_toggle = function()
   -- Prefer treesj on arrow functions
-  if in_arrow_function() then
+  if on_arrow_function() then
     vim.notify("treesj")
     treesj.toggle()
     return
   end
 
   -- Prefer mini for bracket-based constructs
-  if splitjoin.toggle() then
+  if on_pair_char() and splitjoin.toggle() then
     vim.notify("splitjoin")
     return
   end
@@ -41,5 +56,5 @@ local split_join_toggle = function()
   treesj.toggle()
 end
 
-vim.keymap.set({ "n" }, "J", treesj.toggle, { desc = "Split/Join" })
+vim.keymap.set({ "n" }, "J", split_join_toggle, { desc = "Split/Join" })
 vim.keymap.set({ "x" }, "J", treesj.toggle, { desc = "Split/Join" })
