c786390d16b9327a54cfc71045cf96670182f97d
Daniel Tolan <danieltolan@gmail.com>
2026-02-26 18:11:30 -1000

    mini.statusline update

--- lua/mini_plugins/statusline.lua
+++ lua/mini_plugins/statusline.lua
@@ -9,6 +9,47 @@ statusline.setup({
         return { table.concat(vim.fn.filter(tbl, function(_, v) return v ~= nil and v ~= "" end), seperator) }
       end
 
+      local section_git = function(args)
+        local truncate = statusline.is_truncated(args.trunc_width)
+
+        local summary = vim.b.minigit_summary
+        if type(summary) ~= "table" then return "" end
+
+        local branch = summary.head_name
+        if not branch then return "" end
+
+        local icon = args.icon and "" .. " " or ""
+        return truncate and "" or icon .. "%#MiniStatuslineGit#" .. branch .. "%#MiniStatuslineDevinfo#"
+      end
+
+      local section_diff = function(args)
+        local truncate = statusline.is_truncated(args.trunc_width)
+        local summary = vim.b.minidiff_summary
+
+        if type(summary) ~= "table" then return "" end
+
+        local symbols = {
+          add = args.symbols and "%#MiniStatuslineDiffAdd#" or "%#MiniStatuslineDiffAdd#",
+          change = args.symbols and "%#MiniStatuslineDiffChange#" or "%#MiniStatuslineDiffChange#",
+          delete = args.symbols and "%#MiniStatuslineDiffDelete#" or "%#MiniStatuslineDiffDelete#",
+        }
+        local diffs, has_diff = {}, false
+
+        for key, symbol in pairs(symbols) do
+          local count = summary[key]
+          if type(count) == "number" and count > 0 then
+            symbol = args.symbols and symbol .. " " or symbol
+            if not args.symbols then table.insert(diffs, symbol .. count .. "%#MiniStatuslineDevinfo#") end
+            if args.symbols then table.insert(diffs, symbol .. "%#MiniStatuslineDevinfo#" .. count) end
+            has_diff = true
+          end
+        end
+        if not has_diff then return "" end
+
+        local icon = args.icon and "" .. " " or ""
+        return truncate and "" or icon .. table.concat(diffs, "|")
+      end
+
       local function section_diagnostics(args)
         local levels = {
           { name = "ERROR", sign = "" },
@@ -43,9 +84,17 @@ statusline.setup({
         return icon .. table.concat(diagnostics, "|")
       end
 
+      local section_filename = function(args)
+        local truncate = statusline.is_truncated(args.trunc_width)
+
+        if vim.bo.buftype == "toggleterm" or vim.bo.buftype == "terminal" then return "%t" end
+
+        return truncate and "%t %h" or "%f %h"
+      end
+
       local section_filetype = function(args)
         local truncate = statusline.is_truncated(args.trunc_width)
-        if truncate then return "" end
+
         local ft = vim.bo.filetype or "none"
         if ft == "toggleterm" then return nil end
 
@@ -59,71 +108,76 @@ statusline.setup({
 
       local section_shift_width = function(args)
         local truncate = statusline.is_truncated(args.trunc_width)
+
         local shiftwidth = "󰌒 " .. vim.bo.shiftwidth
+
         return truncate and "" or shiftwidth
       end
 
       local section_spell = function(args)
         local truncate = statusline.is_truncated(args.trunc_width)
+
         local spell = vim.wo.spell and "󰓆" or ""
+
         return truncate and "" or spell
+      end
+
+      local section_searchcount = function(args)
+        local truncate = statusline.is_truncated(args.trunc_width)
+
+        local in_search = vim.fn.getcmdtype() == "/" or vim.fn.getcmdtype() == "?"
+        if vim.v.hlsearch == 0 and not in_search then return "" end
+
+        local pattern = in_search and vim.fn.getcmdline() or vim.fn.getreg("/")
+        if pattern == "" then return "" end
+
+        local ok, s_count = pcall(vim.fn.searchcount, { recompute = true, pattern = pattern })
+        if not ok or s_count.current == nil or s_count.total == 0 then return "" end
+
+        if s_count.incomplete == 1 then return "?/?" end
+
+        local too_many = ">" .. s_count.maxcount
+
+        local current = s_count.current > s_count.maxcount and too_many or s_count.current
+        local total = s_count.total > s_count.maxcount and too_many or s_count.total
+
+        return truncate and "" or current .. "/" .. total
       end
 
       local section_location = function(args)
         local truncate = statusline.is_truncated(args.trunc_width)
+
         local ft = vim.bo.filetype or "none"
         if ft == "toggleterm" then return nil end
+
         local line = vim.fn.line(".")
         local col = vim.fn.col(".")
-        return truncate and "" or string.format("%d:%d", line, col)
+
+        return truncate and "" or line .. ":" .. col -- string.format("%d:%d", line, col)
       end
 
       local section_progress = function(args)
         local truncate = statusline.is_truncated(args.trunc_width)
+
         local ft = vim.bo.filetype or "none"
         if ft == "toggleterm" then return nil end
+
         return truncate and "" or "%P"
       end
 
-      local section_diff = function(args)
-        local truncate = statusline.is_truncated(args.trunc_width)
-        local summary = vim.b.minidiff_summary
-
-        if type(summary) ~= "table" then return "" end
-
-        local symbols = {
-          add = args.symbols and "%#MiniStatuslineDiffAdd#" or "%#MiniStatuslineDiffAdd#",
-          change = args.symbols and "%#MiniStatuslineDiffChange#" or "%#MiniStatuslineDiffChange#",
-          delete = args.symbols and "%#MiniStatuslineDiffDelete#" or "%#MiniStatuslineDiffDelete#",
-        }
-        local diffs, has_diff = {}, false
-
-        for key, symbol in pairs(symbols) do
-          local count = summary[key]
-          if type(count) == "number" and count > 0 then
-            symbol = args.symbols and symbol .. " " or symbol
-            if not args.symbols then table.insert(diffs, symbol .. count .. "%#MiniStatuslineDevinfo#") end
-            if args.symbols then table.insert(diffs, symbol .. "%#MiniStatuslineDevinfo#" .. count) end
-            has_diff = true
-          end
-        end
-        if not has_diff then return "" end
-
-        local icon = args.icon and "" .. " " or ""
-        return truncate and "" or icon .. table.concat(diffs, "|")
-      end
-
       local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
-      local git = statusline.section_git({ trunc_width = 80 })
-      local diff = section_diff({ trunc_width = 80, icon = true, symbols = false })
-      local diagnostics = section_diagnostics({ trunc_width = 75, icon = true, symbols = false })
-      local filename = statusline.section_filename({ trunc_width = 120 })
+      local git = section_git({ trunc_width = 70, icon = true })
+      local diff = section_diff({ trunc_width = 70, icon = true, symbols = false })
+      local diagnostics = section_diagnostics({ trunc_width = 70, icon = true, symbols = false })
+      local filename = section_filename({ trunc_width = 120 })
       local filetype = section_filetype({ trunc_width = 120 })
       local shiftwidth = section_shift_width({ trunc_width = 120 })
       local spell = section_spell({ trunc_width = 120 })
-      local searchcount = statusline.section_searchcount({ trunc_width = 80 })
-      local location = section_location({ trunc_width = 80 })
-      local progress = section_progress({ trunc_width = 80 })
+      local searchcount = section_searchcount({ trunc_width = 70 })
+      local location = section_location({ trunc_width = 70 })
+      local progress = section_progress({ trunc_width = 70 })
+
+      local truncate = statusline.is_truncated(70)
 
       return statusline.combine_groups({
         { hl = mode_hl, strings = combine_strings({ mode }, "│") },
@@ -181,4 +235,13 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   group = group,
   desc = "Generate icon highlights",
   callback = apply_hl,
+})
+
+local redraw_status = function() vim.cmd("redrawstatus") end
+
+vim.api.nvim_create_autocmd("CmdlineChanged", {
+  pattern = { "/", "?" },
+  group = group,
+  desc = "Redraw statusline on search",
+  callback = redraw_status,
 })
