faccf4d6e68237fbb0250ba1c427299556794142
Daniel Tolan <danieltolan@gmail.com>
2026-02-26 18:12:52 -1000

    remove mini git summary string autocmd

--- lua/mini_plugins/git.lua
+++ lua/mini_plugins/git.lua
@@ -9,23 +9,3 @@ git.setup({
     split = "auto",
   },
 })
-
--- #############################################################################
--- #                            Automatic Commands                             #
--- #############################################################################
-
-local group = vim.api.nvim_create_augroup("mini_git", { clear = true })
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "MiniGitUpdated",
-  group = group,
-  callback = function(data)
-    local summary = vim.b[data.buf].minigit_summary
-    local branch = summary.head_name
-    if branch then
-      vim.b[data.buf].minigit_summary_string = "%#MiniStatuslineGit#" .. branch .. "%#MiniStatuslineDevinfo#"
-    else
-      vim.b[data.buf].minigit_summary_string = nil
-    end
-  end,
-})
