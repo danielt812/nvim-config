972bd9cf00a6ddd807978a01e34b0443977b57ce
Daniel Tolan <danieltolan@gmail.com>
2026-03-02 09:19:19 -1000

    mini.splitjoin - update

--- lua/mini_plugins/splitjoin.lua
+++ lua/mini_plugins/splitjoin.lua
@@ -16,18 +16,19 @@ local ft_condition = function(hook, disabled)
 end
 
 -- filetypes where trailing commas are invalid / undesirable
-local no_trailing_comma = { json = true, jsonc = true, toml = true, yaml = true, yml = true }
+local no_trailing_comma_curly = { json = true, jsonc = true, toml = true, yaml = true, yml = true }
+local no_trailing_comma_square = { json = true, jsonc = true, toml = true, yaml = true, yml = true }
+local no_trailing_comma_paren = { json = true, jsonc = true, toml = true, yaml = true, yml = true }
 
-local gen_hook = splitjoin.gen_hook
-local add_trailing_separator = gen_hook.add_trailing_separator
-local del_trailing_separator = gen_hook.del_trailing_separator
+local add_trailing_separator = splitjoin.gen_hook.add_trailing_separator
+local del_trailing_separator = splitjoin.gen_hook.del_trailing_separator
 
 -- stylua: ignore start
-local add_comma_curly  = ft_condition(add_trailing_separator(curly), no_trailing_comma)
-local del_comma_curly  = ft_condition(del_trailing_separator(curly), no_trailing_comma)
-local add_comma_square = ft_condition(add_trailing_separator(square), no_trailing_comma)
-local del_comma_square = ft_condition(del_trailing_separator(square), no_trailing_comma)
-local del_comma_paren  = ft_condition(del_trailing_separator(paren), no_trailing_comma)
+local add_comma_curly  = ft_condition(add_trailing_separator(curly), no_trailing_comma_curly)
+local del_comma_curly  = ft_condition(del_trailing_separator(curly), no_trailing_comma_curly)
+local add_comma_square = ft_condition(add_trailing_separator(square), no_trailing_comma_square)
+local del_comma_square = ft_condition(del_trailing_separator(square), no_trailing_comma_square)
+local del_comma_paren  = ft_condition(del_trailing_separator(paren), no_trailing_comma_paren)
 -- stylua: ignore end
 
 local pad_curly = splitjoin.gen_hook.pad_brackets(curly)
