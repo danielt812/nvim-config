4bd340ba8ca53dddf363eb83071c5d20b4f83995
Daniel Tolan <danieltolan@gmail.com>
2026-03-04 13:57:16 -1000

    colors update

--- colors/sonokai.lua
+++ colors/sonokai.lua
@@ -159,7 +159,7 @@ local highlights = {
   -- stylua: ignore start
   Normal   = { fg = palette.fg,    bg = palette.bg0 },
   NormalNC = { fg = palette.fg,    bg = palette.bg0 },
-  Terminal = { fg = palette.white, bg = palette.bg_dim },
+  Terminal = { fg = palette.white, bg = palette.bg_dim }, -- NOTE: CUSTOM
   -- stylua: ignore end
 
   -- stylua: ignore start
@@ -856,7 +856,7 @@ local highlights = {
   MiniIconsGreen   = { link = "Green" },
   MiniIconsGrey    = { link = "Grey" },
   MiniIconsOrange  = { link = "Orange" },
-  MiniIconsPurple = { link = "Purple" },
+  MiniIconsPurple  = { link = "Purple" },
   MiniIconsRed     = { link = "Red" },
   MiniIconsYellow  = { link = "Yellow" },
   -- stylua: ignore end
