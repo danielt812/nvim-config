66fcac51ac22a25ec737fb59b584b89a3e734982
Daniel Tolan <danieltolan@gmail.com>
2026-03-04 13:59:08 -1000

    format docker language server

--- lua/lsp/docker_language_server.lua
+++ lua/lsp/docker_language_server.lua
@@ -9,24 +9,24 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'docker-language-server', 'start', '--stdio' },
-  filetypes = { 'dockerfile', 'yaml.docker-compose' },
+  cmd = { "docker-language-server", "start", "--stdio" },
+  filetypes = { "dockerfile", "yaml" },
   get_language_id = function(_, ft)
-    if ft == 'yaml.docker-compose' or ft:lower():find('ya?ml') then
-      return 'dockercompose'
+    if ft == "yaml.docker-compose" or ft:lower():find("ya?ml") then
+      return "dockercompose"
     else
       return ft
     end
   end,
   root_markers = {
-    'Dockerfile',
-    'docker-compose.yaml',
-    'docker-compose.yml',
-    'compose.yaml',
-    'compose.yml',
-    'docker-bake.json',
-    'docker-bake.hcl',
-    'docker-bake.override.json',
-    'docker-bake.override.hcl',
+    "Dockerfile",
+    "docker-compose.yaml",
+    "docker-compose.yml",
+    "compose.yaml",
+    "compose.yml",
+    "docker-bake.json",
+    "docker-bake.hcl",
+    "docker-bake.override.json",
+    "docker-bake.override.hcl",
   },
 }
