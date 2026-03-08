021ec4a31bcd32421ac667f8ea2fe62fa6981ce9
Daniel Tolan <danieltolan@gmail.com>
2026-03-07 23:39:11 -1000

    mini.jump - hybrid quickscope

--- lua/mini_plugins/jump.lua
+++ lua/mini_plugins/jump.lua
@@ -6,7 +6,7 @@ jump.setup({
     backward = "F",
     forward_till = "t",
     backward_till = "T",
-    repeat_jump = ";",
+    repeat_jump = "",
   },
   delay = {
     highlight = 250,
@@ -19,9 +19,106 @@ jump.setup({
 -- #                                  Keymaps                                  #
 -- #############################################################################
 
-local jump_stop = function()
-  if not jump.state.jumping then return "<esc>" end
-  jump.stop_jumping()
+vim.keymap.set({ "n", "x", "o" }, ";", function() jump.jump(nil, false, nil, vim.v.count1) end)
+
+vim.keymap.set({ "n", "x", "o" }, ",", function() jump.jump(nil, true, nil, vim.v.count1) end)
+
+vim.on_key(function(_, key)
+  if jump.state.jumping and key == vim.keycode("<esc>") then jump.stop_jumping() end
+end, vim.api.nvim_create_namespace("mini_jump_esc"))
+
+-- #############################################################################
+-- #                            Automatic Commands                             #
+-- #############################################################################
+
+local gen_hl_groups = function() vim.api.nvim_set_hl(0, "MiniJumpDim", { link = "NonText" }) end
+
+gen_hl_groups() -- Call this now if colorscheme was already set
+
+local dim_match_ids = {}
+
+local find_boundary = function(line, search, backward, ignore_case)
+  if backward then
+    local lines = vim.api.nvim_buf_get_lines(0, 0, line - 1, false)
+    for i = #lines, 1, -1 do
+      local l = ignore_case and lines[i]:lower() or lines[i]
+      if l:find(search, 1, true) then return i end
+    end
+  else
+    local lines = vim.api.nvim_buf_get_lines(0, line, -1, false)
+    for i = 1, #lines do
+      local l = ignore_case and lines[i]:lower() or lines[i]
+      if l:find(search, 1, true) then return line + i end
+    end
+  end
+  return line
 end
 
-vim.keymap.set({ "n", "x" }, "<esc>", jump_stop, { expr = true })
+local clear_dim_matches = function()
+  for _, id in ipairs(dim_match_ids) do
+    pcall(vim.fn.matchdelete, id)
+  end
+  dim_match_ids = {}
+end
+
+local add_dim_matches = function(line, boundary, backward)
+  local ids = {}
+  local function add(pat) ids[#ids + 1] = vim.fn.matchadd("MiniJumpDim", pat, 11) end
+  if backward then
+    add(string.format([[\%%>%dl]], line))
+    if boundary > 1 then add(string.format([[\%%<%dl]], boundary)) end
+  else
+    add(string.format([[\%%<%dl]], line))
+    if boundary < vim.api.nvim_buf_line_count(0) then add(string.format([[\%%>%dl]], boundary)) end
+  end
+  return ids
+end
+
+local apply_dim = function()
+  clear_dim_matches()
+  local line = vim.fn.line(".")
+  local target = jump.state.target
+  if not target then return end
+
+  local backward = jump.state.backward
+  local ignore_case = vim.o.ignorecase and (not vim.o.smartcase or target == target:lower())
+  local search = ignore_case and target:lower() or target
+  local boundary = find_boundary(line, search, backward, ignore_case)
+
+  dim_match_ids = add_dim_matches(line, boundary, backward)
+end
+
+local jump_dim_cb = function() vim.schedule(apply_dim) end
+
+local jump_undim_cb = clear_dim_matches
+
+local jump_stop_cb = function()
+  if jump.state.jumping then jump.stop_jumping() end
+end
+
+local group = vim.api.nvim_create_augroup("mini_jump", { clear = true })
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "no*:*",
+  group = group,
+  desc = "Stop jump mode after operator-pending mode keymaps",
+  callback = jump_stop_cb,
+})
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  group = group,
+  callback = gen_hl_groups,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniJumpJump",
+  group = group,
+  callback = jump_dim_cb,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniJumpStop",
+  group = group,
+  callback = jump_undim_cb,
+})
