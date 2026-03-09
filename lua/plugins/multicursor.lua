8e3abdfd379d176bf4a4ba4ca7e3b57f1c324d97
Daniel Tolan <danieltolan@gmail.com>
2026-03-09 07:05:01 -1000

    multicursor - update keymaps

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -2,16 +2,20 @@ local multicursor = require("multicursor-nvim")
 multicursor.setup()
 
 -- VM-like “add cursor by line”
+vim.keymap.set({ "n", "x" }, "<C-j>", function() multicursor.lineAddCursor(1) end)
+vim.keymap.set({ "n", "x" }, "<C-k>", function() multicursor.lineAddCursor(-1) end)
 vim.keymap.set({ "n", "x" }, "<Down>", function() multicursor.lineAddCursor(1) end)
-vim.keymap.set({ "n", "x" }, "<Up>",   function() multicursor.lineAddCursor(-1) end)
+vim.keymap.set({ "n", "x" }, "<Up>", function() multicursor.lineAddCursor(-1) end)
 
 -- VM-like “match next/prev”
-vim.keymap.set({ "n", "x" }, "<C-n>", function() multicursor.matchAddCursor(1) end)
 vim.keymap.set({ "n", "x" }, "<C-p>", function() multicursor.matchAddCursor(-1) end)
+-- vim.keymap.set({ "n", "x" }, "<C-p>", function() multicursor.matchSkipCursor(-1) end)
+vim.keymap.set({ "n", "x" }, "<C-n>", function() multicursor.matchAddCursor(1) end)
+vim.keymap.set({ "n", "x" }, "<C-s>", function() multicursor.matchSkipCursor(1) end)
 
 -- Cursor navigation layer (your existing pattern is good)
 multicursor.addKeymapLayer(function(layerSet)
-  layerSet({ "n", "x" }, "<left>",  multicursor.prevCursor)
+  layerSet({ "n", "x" }, "<left>", multicursor.prevCursor)
   layerSet({ "n", "x" }, "<right>", multicursor.nextCursor)
 
   -- VM-ish “remove cursor” (choose a key you like)
@@ -19,11 +23,17 @@ multicursor.addKeymapLayer(function(layerSet)
 
   -- VM-ish escape behavior
   layerSet({ "n", "x" }, "<esc>", function()
-    if multicursor.cursorsEnabled and multicursor.cursorsEnabled() then
+    if multicursor.cursorsEnabled() then
       multicursor.clearCursors()
-    else
-      -- If the plugin doesn't expose cursorsEnabled(), just clear anyway:
-      pcall(multicursor.clearCursors)
     end
   end)
 end)
+
+local gen_hl_groups = function()
+  -- stylua: ignore start
+  vim.api.nvim_set_hl(0, "MultiCursorCursor",       { link = "Search" })
+  vim.api.nvim_set_hl(0, "MultiCursorMatchPreview", { link = "IncSearch" })
+  -- stylua: ignore end
+end
+
+gen_hl_groups()
