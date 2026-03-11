ce0b40db7188484352ee9b8afd2925f9063ad87d
Daniel Tolan <danieltolan@gmail.com>
2026-03-10 19:11:16 -1000

    mini.cmdline - :Peek user cmd

--- lua/mini_plugins/cmdline.lua
+++ lua/mini_plugins/cmdline.lua
@@ -37,10 +37,46 @@ cmdline.setup({
     -- Window options
     window = {
       -- Floating window config
-      config = {},
+      config = function()
+        local peek_mark = vim.g.peek_mark
+        if peek_mark then return { title = " Peek (mark) " .. peek_mark .. " " } end
+        return {}
+      end,
 
       -- Function to render statuscolumn
       statuscolumn = nil,
     },
   },
 })
+
+-- #############################################################################
+-- #                            Automatic Commands                             #
+-- #############################################################################
+
+local group = vim.api.nvim_create_augroup("mini_cmdline", { clear = true })
+
+vim.api.nvim_create_autocmd("CmdlineLeave", {
+  pattern = "*",
+  group = group,
+  desc = "Clear peek_mark",
+  callback = function() vim.g.peek_mark = nil end,
+})
+
+-- #############################################################################
+-- #                               User Commands                               #
+-- #############################################################################
+
+vim.api.nvim_create_user_command("Peek", function(opts)
+  local char = opts.args
+  if not char:match("^[a-z]$") then
+    vim.notify("Peek requires a local mark (a-z)", vim.log.levels.WARN)
+    return
+  end
+  local pos = vim.api.nvim_buf_get_mark(0, char)
+  if pos[1] == 0 then
+    vim.notify("Mark '" .. char .. "' not set", vim.log.levels.WARN)
+    return
+  end
+  vim.g.peek_mark = char
+  vim.api.nvim_input(":" .. pos[1])
+end, { nargs = 1 })
