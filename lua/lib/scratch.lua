3e33bddec99d9ff414bc03557b256e75dc6a16fb
Daniel Tolan <danieltolan@gmail.com>
2026-03-11 22:25:21 -1000

    add typescript to scratch buf list

--- lua/lib/scratch.lua
+++ lua/lib/scratch.lua
@@ -1,6 +1,7 @@
 local scratch_types = {
   { ft = "html" },
   { ft = "javascript", ext = "js" },
+  { ft = "typescript", ext = "ts" },
   { ft = "json" },
   { ft = "lua" },
   { ft = "markdown", ext = "md" },
