a0c8876cba687e80f827fcc57ea946f3d1f8afe9
Daniel Tolan <danieltolan@gmail.com>
2026-03-17 20:44:17 -1000

    conform - set up kulala fmt

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -89,6 +89,9 @@ vim.api.nvim_create_user_command("ConformLog", function()
   vim.cmd("split " .. log_path)
 end, { desc = "Open Conform log file" })
 
+-- kulala-fmt ----------------------------------------------------------------
+conform.formatters_by_ft.http = { "kulala-fmt" }
+
 -- vim.api.nvim_create_autocmd("BufWritePre", {
 --   group = vim.api.nvim_create_augroup("format_on_save", { clear = true }),
 --   desc = "Format on save",
