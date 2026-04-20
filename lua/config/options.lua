81e397d0ef967810692e8e851b696210bf5a2de1
Daniel Tolan <danieltolan@gmail.com>
2026-04-20 08:22:55 -1000

    update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,67 +1,66 @@
--- stylua: ignore start
 -- INFO: File handling
-vim.opt.backup         = false -- Disable backup files
-vim.opt.swapfile       = false -- Disable swap files
-vim.opt.undofile       = true -- Persistent undo
-vim.opt.writebackup    = false -- Disable backup before overwrite
+vim.opt.backup = false -- Disable backup files
+vim.opt.swapfile = false -- Disable swap files
+vim.opt.undofile = true -- Persistent undo
+vim.opt.writebackup = false -- Disable backup before overwrite
 
 -- INFO: Editing behavior
-vim.opt.autoindent     = true -- Copy indent from current line
-vim.opt.breakindent    = true -- Preserve indentation on wrapped lines
-vim.opt.expandtab      = true -- Use spaces instead of tabs
-vim.opt.linebreak      = true -- Wrap at word boundaries
-vim.opt.mouse          = "a" -- Enable mouse support
-vim.opt.shiftwidth     = 2 -- Spaces per indent step
-vim.opt.smartindent    = true -- Make indenting smart
-vim.opt.tabstop        = 2 -- Visual width of a tab
+vim.opt.autoindent = true -- Copy indent from current line
+vim.opt.breakindent = true -- Preserve indentation on wrapped lines
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.linebreak = true -- Wrap at word boundaries
+vim.opt.mouse = "a" -- Enable mouse support
+vim.opt.shiftwidth = 2 -- Spaces per indent step
+vim.opt.smartindent = true -- Make indenting smart
+vim.opt.tabstop = 2 -- Visual width of a tab
 
 -- INFO: Search and grep
-vim.opt.grepprg        = "rg --vimgrep" -- Use ripgrep.rc config
-vim.opt.ignorecase     = true -- Ignore case when searching (use `\C` to force not doing that)
-vim.opt.incsearch      = true -- Show search results while typing
-vim.opt.smartcase      = true -- Don't ignore case when searching if pattern has upper case
+vim.opt.grepprg = "rg --vimgrep" -- Use ripgrep.rc config
+vim.opt.ignorecase = true -- Ignore case when searching (use `\C` to force not doing that)
+vim.opt.incsearch = true -- Show search results while typing
+vim.opt.smartcase = true -- Don't ignore case when searching if pattern has upper case
 
 -- INFO: Completion
-vim.opt.completeopt    = "menuone,noselect,fuzzy" -- Completion popup behavior
-vim.opt.infercase      = true -- Infer letter cases for a richer built-in keyword completion
-vim.opt.pumheight      = 10 -- Max popup menu height
+vim.opt.completeopt = "menuone,noselect,fuzzy" -- Completion popup behavior
+vim.opt.infercase = true -- Infer letter cases for a richer built-in keyword completion
+vim.opt.pumheight = 10 -- Max popup menu height
 
 -- INFO: UI and display
-vim.opt.conceallevel   = 0         -- Show text normally, no concealing
-vim.opt.cursorline     = true      -- Highlight current line
-vim.opt.laststatus     = 3         -- Global statusline
-vim.opt.list           = true      -- Show invisible characters
-vim.opt.number         = true      -- Show absolute line numbers
-vim.opt.relativenumber = true      -- Show relative line numbers
-vim.opt.ruler          = false     -- Don't show cursor position in command line
-vim.opt.scrolloff      = 10        -- Keep context lines above/below cursor
-vim.opt.showmode       = false     -- Don't show mode in command line
-vim.opt.signcolumn     = "yes"     -- Always show sign column
-vim.opt.winborder      = "rounded" -- Use rounded floating window borders
-vim.opt.wrap           = false     -- Display long lines as just one line
+vim.opt.conceallevel = 0 -- Show text normally, no concealing
+vim.opt.cursorline = true -- Highlight current line
+vim.opt.laststatus = 3 -- Global statusline
+vim.opt.list = true -- Show invisible characters
+vim.opt.number = true -- Show absolute line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
+vim.opt.ruler = false -- Don't show cursor position in command line
+vim.opt.scrolloff = 10 -- Keep context lines above/below cursor
+vim.opt.showmode = false -- Don't show mode in command line
+vim.opt.signcolumn = "yes" -- Always show sign column
+vim.opt.winborder = "rounded" -- Use rounded floating window borders
+vim.opt.wrap = false -- Display long lines as just one line
 
 -- INFO: Window splits
-vim.opt.splitbelow     = true -- Horizontal splits will be below
-vim.opt.splitright     = true -- Vertical splits will be to the right
+vim.opt.splitbelow = true -- Horizontal splits will be below
+vim.opt.splitright = true -- Vertical splits will be to the right
 
 -- INFO: Folding
-vim.opt.foldcolumn     = "0" -- Hide dedicated fold column
-vim.opt.foldenable     = true -- Enable folding
-vim.opt.foldlevel      = 99 -- Open folds up to this level
+vim.opt.foldcolumn = "0" -- Hide dedicated fold column
+vim.opt.foldenable = true -- Enable folding
+vim.opt.foldlevel = 99 -- Open folds up to this level
 vim.opt.foldlevelstart = 99 -- Start with folds open
-vim.opt.foldmethod     = "expr"
-vim.opt.foldexpr       = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldtext       = "v:lua.foldtext()"
+vim.opt.foldmethod = "expr" -- Use expression for folding
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- Treesitter-based fold expression
+vim.opt.foldtext = "v:lua.foldtext()" -- Custom fold text function
 
 -- INFO: Spell
-vim.opt.spellfile      = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
+vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add" -- Custom spell file path
 
 -- INFO: System and performance
-vim.opt.clipboard      = "unnamedplus" -- Sync with system clipboard
-vim.opt.updatetime     = 1000 -- CursorHold and swap write interval (ms)
+vim.opt.clipboard = "unnamed" -- Sync with system clipboard
+vim.opt.updatetime = 1000 -- CursorHold and swap write interval (ms)
 vim.opt.shortmess:append("S") -- Don't show search count
-vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:ver25-TermCursor"
--- stylua: ignore end
 
-vim.hl.priorities.treesitter = 100
-vim.hl.priorities.semantic_tokens = 125
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:ver25-TermCursor" -- Cursor shape per mode
+
+vim.hl.priorities.treesitter = 100 -- Treesitter highlight priority
+vim.hl.priorities.semantic_tokens = 125 -- Semantic tokens override treesitter
