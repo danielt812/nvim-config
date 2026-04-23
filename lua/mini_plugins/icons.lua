f574e54dfe995fd9e452a06095cdf3533a61e4d3
Daniel Tolan <danieltolan@gmail.com>
2026-04-23 07:32:12 -1000

    gear icon for .env

--- lua/mini_plugins/icons.lua
+++ lua/mini_plugins/icons.lua
@@ -46,6 +46,7 @@ icons.setup({
     [".dockerignore"] = { glyph = "󰡨", hl = "MiniIconsAzure" },
     [".gitignore"]    = { glyph = "󰊢", hl = "MiniIconsOrange" },
     [".shellcheckrc"] = { glyph = "󰒓", hl = "MiniIconsGrey" },
+    [".env"]          = { glyph = "󰒓", hl = "MiniIconsGrey" },
     -- stylua: ignore end
   },
   filetype = {
